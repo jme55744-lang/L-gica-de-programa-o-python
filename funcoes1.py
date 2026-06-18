d7ce4b8fb22ff5085b09ec53c55e183b29ebbb85
jme55744-lang <jme55744@gmail.com>
2026-06-17 22:22:11 -0300

    exercicio 1

---
+++ funcoes1.py
@@ -0,0 +1,8 @@
+def somar_valores(parametro1, parametro2): 
+    """docstring: descreve o que a função faz"""
+    #corpo da função
+    resultado = parametro1 + parametro2
+    return resultado
+
+soma = somar_valores(5, 6)
+print(soma)
---
+++ funcoes2.py
@@ -0,0 +1,6 @@
+def imprime_nome(nome):
+    print("Nome:", nome)
+    #Seme return explícito
+
+resultado = imprime_nome("Ana")
+print(resultado)
